7cacff03fe996137650741cf4c82355b21cb9f88
happyzhenya <happy.zhenya@gmail.com>
2022-04-29 22:09:49 +0300

    feat: crate stack

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+ 
+  constructor(){
+    this.stack=[];
+
+  }
+
+  push(element) {
+    this.stack.push(element);
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+   if (this.stack.length===0) return null;
+   return this.stack.pop() 
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+   return this.stack[this.stack.length-1];
     // remove line with error and write your code here
   }
 }
